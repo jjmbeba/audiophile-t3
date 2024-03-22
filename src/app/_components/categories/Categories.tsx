7b5c7a4beb17ee7ecb2b38854ddb602e2fac0fe6
Johnson Mbeba <jjmbeba@gmail.com>
2024-03-23 00:37:40 +0300

    Add desktop categories ui

--- src/app/_components/categories/Categories.tsx
+++ src/app/_components/categories/Categories.tsx
@@ -2,6 +2,7 @@ import { ChevronRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { AspectRatio } from "~/components/ui/aspect-ratio";
 
 type Category = {
   title: string;
@@ -41,19 +42,19 @@ export default Categories;
 
 const CategoryCard = ({ title, link, imageUrl }: Category) => {
   return (
-    <div className="relative flex flex-col items-center rounded-[0.5rem] bg-[#f1f1f1] pb-[1.625rem]">
+    <div className="relative flex flex-col items-center rounded-[0.5rem] bg-[#f1f1f1] pb-[1.625rem] lg:pb-[2.125rem]">
       <Image
         src={imageUrl}
-        className="absolute -top-[3.25rem] z-0 bg-transparent"
+        className="absolute -top-[3.25rem] z-0 bg-transparent lg:h-[10rem] lg:w-[10.684375rem]"
         width={147}
         height={133}
         alt={title}
       />
-      <p className="z-10 mt-[5.8125rem] text-center text-[0.9375rem] font-bold leading-[1.5625rem]">
+      <p className="z-10 mt-[5.8125rem] text-center text-[0.9375rem] font-bold leading-[1.5625rem] lg:mt-[7.625rem] lg:text-[1.125rem] lg:tracking-[0.08125rem]">
         {title}
       </p>
       <Link href={link}>
-        <div className="sub-title mt-[1.5625rem] flex items-center gap-[0.8125rem] text-center text-[#797979]">
+        <div className="sub-title mt-[1.5625rem] flex items-center gap-[0.8125rem] text-center text-[#797979] hover:text-primary">
           SHOP <ChevronRight className="h-4 w-4 text-primary" />
         </div>
       </Link>
