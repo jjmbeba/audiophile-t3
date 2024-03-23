68d7f9991ef58dc0a7179df0a15898bc7564ac2a
Johnson Mbeba <jjmbeba@gmail.com>
2024-03-23 12:59:56 +0300

    Add desktop cta ui

--- src/app/_components/home/cta/CTA.tsx
+++ src/app/_components/home/cta/CTA.tsx
@@ -31,8 +31,8 @@ const CTA = () => {
         : "mobile";
 
   return (
-    <div className="mt-[7.5rem] flex flex-col items-center *:text-center md:mt-[6rem]">
-      <div className="relative h-[18.75rem] w-full">
+    <section className="mt-[7.5rem] flex flex-col items-center pb-[7.75rem] *:text-center md:mt-[6rem] md:pb-[6.1875rem] lg:mt-[12.5rem] lg:flex-row-reverse lg:gap-[2.875rem] lg:pb-[12.5rem]">
+      <div className="relative h-[18.75rem] w-full lg:h-[34.75rem] lg:w-[40.75rem]">
         <Image
           src={`/shared/${screen}/image-best-gear.jpg`}
           fill
@@ -40,18 +40,20 @@ const CTA = () => {
           className="rounded-[0.5rem]"
         />
       </div>
-      <h4 className="mt-[3.125rem] uppercase md:mt-[4.4375rem] md:max-w-[35.8125rem] md:text-[2.5rem] md:leading-[2.75rem] md:tracking-[0.09375rem]">
-        Bringing you the <span className="text-primary">best</span> audio gear
-      </h4>
-      <p className="mt-[2.9375rem] text-[#7d7d7d] md:max-w-[35.8125rem]">
-        Located at the heart of New York City, Audiophile is the premier store
-        for high end headphones, earphones, speakers, and audio accessories. We
-        have a large showroom and luxury demonstration rooms available for you
-        to browse and experience a wide range of our products. Stop by our store
-        to meet some of the fantastic people who make Audiophile the best place
-        to buy your portable audio equipment.
-      </p>
-    </div>
+      <div className="mt-[3.125rem] md:mt-[4.4375rem] md:max-w-[35.8125rem] lg:max-w-[27.8125rem] lg:text-left">
+        <h4 className="uppercase md:text-[2.5rem] md:leading-[2.75rem] md:tracking-[0.09375rem]">
+          Bringing you the <span className="text-primary">best</span> audio gear
+        </h4>
+        <p className="mt-[2.9375rem] text-[#7d7d7d]">
+          Located at the heart of New York City, Audiophile is the premier store
+          for high end headphones, earphones, speakers, and audio accessories.
+          We have a large showroom and luxury demonstration rooms available for
+          you to browse and experience a wide range of our products. Stop by our
+          store to meet some of the fantastic people who make Audiophile the
+          best place to buy your portable audio equipment.
+        </p>
+      </div>
+    </section>
   );
 };
 
