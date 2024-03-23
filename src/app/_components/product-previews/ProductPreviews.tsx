ed118129afbd7e1894587409279b78d8981e9239
Johnson Mbeba <jjmbeba@gmail.com>
2024-03-23 12:18:44 +0300

    Add destkop product previews ui

--- src/app/_components/product-previews/ProductPreviews.tsx
+++ src/app/_components/product-previews/ProductPreviews.tsx
@@ -33,7 +33,7 @@ const ProductPreviews = () => {
         : "mobile";
 
   return (
-    <section className="mt-[7.5rem] flex flex-col gap-[1.5rem]">
+    <section className="mt-[7.5rem] flex flex-col gap-[1.5rem] lg:gap-[3rem]">
       <ZX9SpeakerPreview screen={screen} />
       <ZX7SpeakerPreview screen={screen} />
       <YX1EarphonesPreview screen={screen} />
@@ -44,34 +44,36 @@ const ProductPreviews = () => {
 export default ProductPreviews;
 const ZX9SpeakerPreview = ({ screen }: { screen: string }) => {
   return (
-    <div className="flex flex-col items-center rounded-[0.5rem] bg-primary pb-[3.4375rem]">
+    <div className="flex flex-col items-center rounded-[0.5rem] bg-primary pb-[3.4375rem] md:pb-0 lg:flex-row lg:justify-center lg:gap-[9.4375rem]">
       <Image
         src={`/home/${screen}/image-speaker-zx9.png`}
         width={172.25}
         height={207}
         alt="zx9-speaker"
-        className="mt-[3.9375rem]"
+        className="mt-[3.9375rem] lg:h-[30.8125rem] lg:w-[25.639375rem]"
       />
-      <p className="mt-[2.9375rem] px-[2.7175rem]  text-center text-[2.25rem] font-bold leading-[2.5rem] tracking-[0.080625rem] text-white md:max-w-[16.3125rem]">
-        ZX9 SPEAKER
-      </p>
-      <p className=" mt-[2.3125rem] px-[1.7175rem] text-center text-[#f5ded2] md:max-w-[21.8125rem]">
-        Upgrade to premium speakers that are phenomenally built to deliver truly
-        remarkable sound.
-      </p>
-      <Button
-        asChild
-        className="mt-[1.9375rem] bg-black px-[2rem] py-[1.1875rem]"
-      >
-        <Link href={"/speakers/zx9-speaker"}>SEE PRODUCT</Link>
-      </Button>
+      <div className="flex flex-col items-center lg:items-start">
+        <p className="mt-[2.9375rem] px-[2.7175rem]  text-center text-[2.25rem] font-bold uppercase leading-[2.5rem] tracking-[0.080625rem] text-white md:max-w-[16.3125rem] lg:px-0 lg:text-left lg:text-[3.5rem] lg:leading-[3.625rem] lg:tracking-[0.125rem]">
+          ZX9 SPEAKER
+        </p>
+        <p className=" mt-[2.3125rem] px-[1.7175rem] text-center text-[#f5ded2] md:max-w-[21.8125rem] lg:px-0 lg:text-left">
+          Upgrade to premium speakers that are phenomenally built to deliver
+          truly remarkable sound.
+        </p>
+        <Button
+          asChild
+          className="mt-[1.9375rem] bg-black px-[2rem] py-[1.1875rem] hover:bg-[#4c4c4c]"
+        >
+          <Link href={"/speakers/zx9-speaker"}>SEE PRODUCT</Link>
+        </Button>
+      </div>
     </div>
   );
 };
 
 const ZX7SpeakerPreview = ({ screen }: { screen: string }) => {
   return (
-    <div className="relative rounded-[0.5rem] px-[1.625rem] py-[6.3125rem]">
+    <div className="relative rounded-[0.5rem] px-[1.625rem] py-[6.3125rem] lg:pt-[6.9375rem]">
       <Image
         src={`/home/${screen}/image-speaker-zx7.jpg`}
         fill
@@ -81,7 +83,7 @@ const ZX7SpeakerPreview = ({ screen }: { screen: string }) => {
       <h4>ZX7 SPEAKER</h4>
       <Button
         variant={"outline"}
-        className="mt-[2.5rem] border-black bg-transparent"
+        className="mt-[2.5rem] border-black bg-transparent hover:bg-[#4c4c4c]"
       >
         <Link href={"/speakers/zx7-speaker"}>SEE PRODUCT</Link>
       </Button>
@@ -91,8 +93,8 @@ const ZX7SpeakerPreview = ({ screen }: { screen: string }) => {
 
 const YX1EarphonesPreview = ({ screen }: { screen: string }) => {
   return (
-    <div className="items-center gap-[0.6875rem] md:flex">
-      <div className="relative h-[12.5rem] w-full md:h-[20rem] md:w-1/2">
+    <div className="items-center gap-[0.6875rem] md:flex lg:gap-[1.875rem]">
+      <div className="relative h-[12.5rem] w-full md:h-[20rem] md:w-1/2 lg:h-[20rem]">
         <Image
           src={`/home/${screen}/image-earphones-yx1.jpg`}
           fill
@@ -104,7 +106,7 @@ const YX1EarphonesPreview = ({ screen }: { screen: string }) => {
         <h4>YX1 EARPHONES</h4>
         <Button
           variant={"outline"}
-          className="mt-[2.5rem] border-black bg-transparent"
+          className="mt-[2.5rem] border-black bg-transparent hover:bg-[#4c4c4c]"
         >
           <Link href={"/earphones/yx1-earphones"}>SEE PRODUCT</Link>
         </Button>
