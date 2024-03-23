e318271b0fb5dbaf99581fbc75f6f01019bdc4ff
Johnson Mbeba <jjmbeba@gmail.com>
2024-03-24 01:33:55 +0300

    Add tablet footer ui

--- src/app/_components/common/footer/Footer.tsx
+++ src/app/_components/common/footer/Footer.tsx
@@ -5,20 +5,20 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="relative flex flex-col items-center bg-black px-6 pb-[2.375rem] pt-[3.625rem] text-white after:absolute after:left-1/2 after:top-0 after:h-[0.25rem] after:w-[6.3125rem] after:-translate-x-[50%] after:bg-primary after:content-[''] lg:px-[10.3125rem]">
+    <footer className="relative flex flex-col items-center bg-black px-6 pb-[2.375rem] pt-[3.625rem] text-white after:absolute after:left-1/2 after:top-0 after:h-[0.25rem] after:w-[6.3125rem] after:-translate-x-[50%] after:bg-primary after:content-['']  md:items-start  md:pb-[3.3125rem] md:pt-[3.8125rem] md:after:left-[2.4375rem] md:after:translate-x-0 lg:px-[10.3125rem]">
       <div>
         <Logo />
-        <NavLinks className="mt-[3.4375rem] flex flex-col gap-[2rem]" />
+        <NavLinks className="mt-[3.4375rem] flex flex-col gap-[2rem] md:mt-[2.5rem] md:flex-row" />
       </div>
-      <div>
-        <div className="mt-[3.9375rem] text-center text-[#878787]">
+      <div className="items-end md:flex">
+        <div className="mt-[3.9375rem] text-center text-[#878787] md:mt-[2.9375rem] md:text-left">
           <p>
-            Audiophile is an all in one stop to fulfill your audio needs. We&apos;re
-            a small team of music lovers and sound specialists who are devoted
-            to helping you get the most out of personal audio. Come and visit
-            our demo facility - we&apos;re open 7 days a week.
+            Audiophile is an all in one stop to fulfill your audio needs.
+            We&apos;re a small team of music lovers and sound specialists who
+            are devoted to helping you get the most out of personal audio. Come
+            and visit our demo facility - we&apos;re open 7 days a week.
           </p>
-          <p className="mt-[3.625rem]">
+          <p className="mt-[3.625rem] md:mt-[5.625rem]">
             Copyright {new Date().getFullYear()}. All Rights Reserved
           </p>
         </div>
