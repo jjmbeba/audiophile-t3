25947278095a21fe9e0405ccacc148457d43a171
Johnson Mbeba <jjmbeba@gmail.com>
2024-04-06 21:57:55 +0300

    mod: refactor auth ui and logic

--- src/app/_components/common/navbar/Navbar.tsx
+++ src/app/_components/common/navbar/Navbar.tsx
@@ -1,10 +1,12 @@
-import { currentUser } from "@clerk/nextjs";
+import { SignInButton, SignUpButton, currentUser } from "@clerk/nextjs";
 import Logo from "../Logo";
 import NavLinks from "../NavLinks";
 import Cart from "./Cart";
 import MobileMenu from "./MobileMenu";
 import User from "./User";
 import Link from "next/link";
+import { Button } from "~/components/ui/button";
+import { MoveRight } from "lucide-react";
 
 const Navbar = async () => {
   const user = await currentUser();
@@ -17,9 +19,28 @@ const Navbar = async () => {
       {user?.id ? (
         <Cart />
       ) : (
-        <Link href={'/signup'}>
-          <User />
-        </Link>
+        <>
+          {/* <Link href={"/signup"} className="md:hidden">
+            <User />
+          </Link> */}
+          <div  className="lg:hidden">
+            <SignUpButton mode="modal">
+              <User />
+            </SignUpButton>
+          </div>
+          <div className="lg:flex items-center gap-2 hidden">
+            <SignUpButton mode="modal">
+              <Button className="rounded-[0.3rem] bg-white px-2 py-1 capitalize text-black">
+                <MoveRight className="mr-2 h-4 w-4" />
+                Get started
+              </Button>
+            </SignUpButton>
+            {/* <SignInButton mode="modal">
+              <Button className="rounded-[0.3rem] px-4 py-3 capitalize text-white bg-transparent" variant={'outline'}>
+                Login</Button>
+            </SignInButton> */}
+          </div>
+        </>
       )}
     </div>
   );
