46de5f4dc2a28c1a43bc02f84f5c771b96894aa2
Johnson Mbeba <jjmbeba@gmail.com>
2024-04-06 20:52:02 +0300

    feat: Add scrollable property to sidebar

--- src/app/_components/common/Sidebar.tsx
+++ src/app/_components/common/Sidebar.tsx
@@ -12,6 +12,7 @@ import {
 } from "~/components/ui/sheet";
 import Categories from "../home/categories/Categories";
 import { useSidebarStore } from "~/store/sidebarStore";
+import { ScrollArea } from "~/components/ui/scroll-area";
 
 const Sidebar = () => {
   const [openSidebarState, setSidebarState] = useSidebarStore((state) => [
@@ -25,11 +26,11 @@ const Sidebar = () => {
         <Menu className="h-6 w-6 lg:hidden" />
       </SheetTrigger>
       <SheetContent side={"top"} className="mt-[5.5625rem] rounded-b-[0.5rem]">
-        <SheetHeader>
+        <ScrollArea className="h-[80vh]">
           <SheetDescription>
             <Categories />
           </SheetDescription>
-        </SheetHeader>
+        </ScrollArea>
       </SheetContent>
     </Sheet>
   );
