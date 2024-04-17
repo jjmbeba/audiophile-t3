ed5cb4755b757a34ba9a0a9d5c53a1f8e78b5fbb
Johnson Mbeba <jjmbeba@gmail.com>
2024-04-17 06:56:34 +0300

    fix: stripe.ts nullish error 1

--- src/app/api/v1/stripe/route.ts
+++ src/app/api/v1/stripe/route.ts
@@ -4,38 +4,5 @@ import { env } from "~/env";
 import { stripe } from "~/lib/stripe";
 
 export async function POST(req: Request) {
-  // const body = await req.text();
-  // const sig = headers().get("Stripe-Signature") as string;
 
-  // const webhookSecret = env.STRIPE_SECRET_KEY;
-
-  // let event: Stripe.Event;
-  // try {
-  //   if (!sig || !webhookSecret) {
-  //     throw new Error("Missing Stripe signature or webhook secret");
-  //   }
-
-  //   event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
-  // } catch (error) {
-  //   console.log(error);
-
-  //   return new Response("Invalid stripe webhook request", {
-  //     status: 400,
-  //   });
-  // }
-
-  // const eventType = event.type;
-
-  // switch (eventType) {
-  //   case "checkout.session.completed":
-  //     const session = event.data.object;
-  //     console.log(`Checkout session completed: ${session.id}`);
-  //     break;
-  //   default:
-  //     console.log(`Unhandled event type ${eventType}`);
-  // }
-
-  // return new Response("OK", {
-  //   status:200
-  // })
 }
