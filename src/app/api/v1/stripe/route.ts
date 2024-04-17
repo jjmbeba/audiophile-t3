9965ac2ce252debf9b164cdedc3a6594292cac2a
Johnson Mbeba <jjmbeba@gmail.com>
2024-04-17 06:59:45 +0300

    rm: stripe.ts and v1 api route

--- src/app/api/v1/stripe/route.ts
+++
@@ -1,8 +0,0 @@
-import { headers } from "next/headers";
-import Stripe from "stripe";
-import { env } from "~/env";
-import { stripe } from "~/lib/stripe";
-
-export async function POST(req: Request) {
-
-}
